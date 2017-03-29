a0ef60e24e8bfbe95a4fc3f5df49a92b7a634727
meristakovska <ms224dm@student.lnu.se>
2017-03-29 15:47:10 +0200

    Changes to eslint config for ecma5

--- server/app.js
+++ server/app.js
@@ -1,25 +1,32 @@
-const express = require("express");
+const express = require('express');
 const exphbs = require('express-handlebars');
-const http = require("http");
-const path = require("path");
-const morgan = require("morgan");
-const routes = require("../client/js/routes.js");
+const http = require('http');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const routes = require('../client/js/routes.js');
 
 const port = 8080;
 const app = express();
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use(morgan('combined'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const hbs = exphbs.create({
   helpers: {},
-  defaultLayout: "main"
+  defaultLayout: 'main'
 });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.use("/", routes);
+app.use('/', routes);
 
-const server = http.createServer(app).listen(port, function() {
-  console.log("Express started on https://localhost:" + port);
-  console.log("Press Ctrl-C to terminate...");
+http.createServer(app).listen(port, function () {
+  console.log('Express started on https://localhost:' + port);
+  console.log('Press Ctrl-C to terminate...');
 });
