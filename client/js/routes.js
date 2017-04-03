27fa493322f8576d080de055172ecc3cacb0b1fa
Toubic <tj22fe@student.lnu.se>
2017-04-03 11:35:40 +0200

    change package.json and - js directory

--- client/js/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.route('/home').get(function (request, response, next) {
-  response.status(200).render('home');
-});
-
-
-module.exports = router;
