2ddad90042768d1584453959ac0e2d87424eac82
Peter Andersson <pa222ku@student.lnu.se>
2017-04-10 12:01:05 +0200

    Fix test path

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar -Dwebdriver.gecko.driver=./tests/geckodriver_0_11.exe ./tests/selenium-server-standalone-3.0.1.jar &
+java -jar -Dwebdriver.gecko.driver=tests/geckodriver0_11.exe tests/selenium-server-standalone-3.0.1.jar &
 SELENIUM_PID=$!
 
 node tests/tr.js
