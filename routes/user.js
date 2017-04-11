52c79551952e1d329699165c8cbcaa536db171f8
meristakovska <ms224dm@student.lnu.se>
2017-04-11 21:03:33 +0200

    Navigation on login

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,20 @@ router.get('/', ensureLoggedIn, function (req, res, next) {
   res.render('user', { user: req.user });
 });
 
+router.get('/dashboard', ensureLoggedIn, function (req, res, next) {
+  res.render('dashboard', { user: req.user });
+});
+
+router.get('/preview', ensureLoggedIn, function (req, res, next) {
+  res.render('preview', { user: req.user });
+});
+
+router.get('/reports', ensureLoggedIn, function (req, res, next) {
+  res.render('reports', { user: req.user });
+});
+
+router.get('/report/:id', ensureLoggedIn, function (req, res, next) {
+  res.render('report', { user: req.user });
+});
+
 module.exports = router;
