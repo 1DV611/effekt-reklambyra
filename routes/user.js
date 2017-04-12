944220d7d31b6d72cb8971240c75b511b9235d7e
meristakovska <ms224dm@student.lnu.se>
2017-04-12 12:54:50 +0200

    Passing information from dashboard to preview

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,22 @@ const passport = require('passport');
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const router = express.Router();
 
+var customerReportSettings = require('../client/js/lib/customerReportSettings.js');
+
+
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function (req, res, next) {
   res.render('user', { user: req.user });
 });
 
 router.get('/dashboard', ensureLoggedIn, function (req, res, next) {
+/*
+  Skicka objekt med information från Auth0 om vilka sociala medier som är aktiverade för användaren
+*/
   res.render('dashboard', { user: req.user });
 });
+
+router.post('/preview', ensureLoggedIn, customerReportSettings);
 
 router.get('/preview', ensureLoggedIn, function (req, res, next) {
   res.render('preview', { user: req.user });
