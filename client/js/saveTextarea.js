28a330bb9aac1a1db671ebed6ae44c324da146be
Toubic <tj22fe@student.lnu.se>
2017-04-30 13:50:57 +0200

    adjust saveTextarea.js

--- client/js/saveTextarea.js
+++ client/js/saveTextarea.js
@@ -1,33 +1,33 @@
 function saveSummary() {
-    var summary = document.querySelector("#summary");
-    var summaryTextarea = document.querySelector("#summaryTextarea");
-    var summaryText = summaryTextarea.value;
-    var summaryButton = document.querySelector("#summaryButton");
+  var summary = document.querySelector('#summary');
+  var summaryTextarea = document.querySelector('#summaryTextarea');
+  var summaryText = summaryTextarea.value;
+  var summaryButton = document.querySelector('#summaryButton');
 
-    summaryButton.classList.add("hidden");
+  summaryButton.classList.add('hidden');
 
-    var text = document.createTextNode(summaryText);
-    var pElement = document.createElement("p");
-    pElement.appendChild(text);
+  var text = document.createTextNode(summaryText);
+  var pElement = document.createElement('p');
+  pElement.appendChild(text);
 
-    summary.appendChild(pElement);
+  summary.appendChild(pElement);
 
-    summaryTextarea.parentNode.removeChild(summaryTextarea);
+  summaryTextarea.parentNode.removeChild(summaryTextarea);
 }
 
 function saveRecommendation() {
-    var recommendation = document.querySelector("#recommendation");
-    var recommendationTextarea = document.querySelector("#recommendationTextarea");
-    var recommendationText = recommendationTextarea.value;
-    var recommendationButton = document.querySelector("#recommendationButton");
+  var recommendation = document.querySelector('#recommendation');
+  var recommendationTextarea = document.querySelector('#recommendationTextarea');
+  var recommendationText = recommendationTextarea.value;
+  var recommendationButton = document.querySelector('#recommendationButton');
 
-    recommendationButton.classList.add("hidden");
+  recommendationButton.classList.add('hidden');
 
-    var text = document.createTextNode(recommendationText);
-    var pElement = document.createElement("p");
-    pElement.appendChild(text);
+  var text = document.createTextNode(recommendationText);
+  var pElement = document.createElement('p');
+  pElement.appendChild(text);
 
-    recommendation.appendChild(pElement);
+  recommendation.appendChild(pElement);
 
-    recommendationTextarea.parentNode.removeChild(recommendationTextarea);
+  recommendationTextarea.parentNode.removeChild(recommendationTextarea);
 }
