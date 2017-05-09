05f915c9ab6bdd2499ba47899445a25d8379941e
Toubic <tj22fe@student.lnu.se>
2017-05-09 13:58:03 +0200

    Clean up redundant routes

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,6 @@ router.get('/dashboard', ensureLoggedIn, function (req, res, next) {
   res.render('dashboard', { user: req.user });
 });
 
-
 router.post('/preview', ensureLoggedIn); //customerReportSettings removed customer report settings
 
 router.get('/preview', ensureLoggedIn, function (req, res, next) {
@@ -35,9 +34,9 @@ router.get('/report/:id', ensureLoggedIn, function (req, res, next) {
 });
 
 router.get('/dashboard', ensureLoggedIn, function (req, res, next) {
-/*
-  Skicka objekt med information från Auth0 om vilka sociala medier som är aktiverade för användaren
-*/
+  /*
+    Skicka objekt med information från Auth0 om vilka sociala medier som är aktiverade för användaren
+  */
   res.render('dashboard', { user: req.user });
 });
 
@@ -48,18 +47,5 @@ router.get('/example', ensureLoggedIn, resultsFromGoogleApi); //resultsFromGoogl
 router.get('/settings', ensureLoggedIn, function (req, res, next) {
   res.render('settings', { user: req.user });
 });
-
-router.get('/preview', ensureLoggedIn, function (req, res, next) {
-  res.render('preview', { user: req.user });
-});
-
-router.get('/reports', ensureLoggedIn, function (req, res, next) {
-  res.render('reports', { user: req.user });
-});
-
-router.get('/report/:id', ensureLoggedIn, function (req, res, next) {
-  res.render('report', { user: req.user });
-});
-
 
 module.exports = router;
