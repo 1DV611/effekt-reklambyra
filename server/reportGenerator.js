ab906702d583e2c0bb7a7f5b03c8f31932e65c04
Peter Andersson <pa222ku@student.lnu.se>
2017-05-12 10:30:44 +0200

    Switch to phantom-html-to-pdf

--- server/reportGenerator.js
+++ server/reportGenerator.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 var handlebars = require('handlebars');
-var pdf = require('html-pdf');
 var inline = require('inline-source');
 var path = require('path');
+
+
+//var streamPdf = htmlPdf;
+var streamPdf = phantomHtmlToPdf;
 
 function reportGenerator(req, res, next) {
   var context = {form : req.app.locals.reportConfig };
   fs.readFile('./views/preview.handlebars', 'utf-8', function (err, data) {
     var template = handlebars.compile(data);
     var html = template(context);
-    var pdfconfig = {
-      orientation: 'landscape'
-    };
     var external_refs = " \
         <link inline href='css/light-bootstrap-dashboard.css'> \
         <link inline href='css/toogle-switch.css'> \
@@ -20,12 +20,28 @@ function reportGenerator(req, res, next) {
         <script inline src='js/lib/charts/createData.js'></script> \
     ";
     inline(external_refs, { rootpath: path.resolve('client') }, function (err, inlines) {
-      pdf.create(inlines+html, pdfconfig).toStream(function(err, stream) {
-        res.setHeader('Content-disposition', 'attachment; filename=rapport.pdf');
-        res.setHeader('Content-type', 'application/pdf');
-        stream.pipe(res);
-      });
+      res.setHeader('Content-disposition', 'attachment; filename=rapport.pdf');
+      res.setHeader('Content-type', 'application/pdf');
+      streamPdf(inlines+html, res);
     });
   });
 }
+
+function htmlPdf(html, res) {
+  var pdf = require('html-pdf');
+  var config = {
+    orientation: 'landscape'
+  };
+  pdf.create(html, config).toStream(function(err, stream) {
+    stream.pipe(res);
+  });
+}
+
+function phantomHtmlToPdf(html, res) {
+  var pdf2 = require('phantom-html-to-pdf')();
+  pdf2({html: html}, function(err, f) {
+    f.stream.pipe(res);
+  });
+}
+
 module.exports = reportGenerator;
