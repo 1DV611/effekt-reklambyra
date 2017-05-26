b89e869a4cd83b702c3cd7856f3a8741ee257c06
LW <lw222ii@student.lnu.se>
2017-05-26 19:29:16 +0800

    rewrote instagram to support date and more stats

--- server/APIs/instagramAPI.js
+++ server/APIs/instagramAPI.js
@@ -1,5 +1,6 @@
 'use strict';
 var dotenv = require('dotenv');
+var epochToDate = require('../helpers/epochToDate');
 dotenv.load();
 
 //https://github.com/jlobos/neo-instagram
@@ -8,77 +9,58 @@ var client = new Instagram({
   client_id: process.env.INSTAGRAM_CLIENT_ID,
   client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
 });
+var relevantDate;
 
-//todo
 /**
  * Even though our access tokens do not specify an expiration time, your app should handle the case that either the user revokes access, or Instagram expires the token after some period of time. If the token is no longer valid, API responses will contain an “error_type=OAuthAccessTokenException”. In this case you will need to re-authenticate the user to obtain a new valid token.
  In other words: do not assume your access_token is valid forever.
  * @param access
+ * @timeStamp something
  */
 
-module.exports = function (access) {
+module.exports = function (access, timeStamp) {
   var tokenObj = {access_token: access.accessToken};
   var userId = access.extraParams.user.id.toString();
+  relevantDate = epochToDate(timeStamp);
 
   return new Promise(function (resolve, reject) {
 
     client.get('users/self/media/recent', tokenObj).then(function (user) {
-
-      var returnObj = {
-        instagram: {
-          totalLikes: totalLikesCount(user.data)
-        }
-      };
-
+      var returnObj = { instagram: filterByMonthYear(user.data)};
       resolve(returnObj);
-      /**
-       * { data:
-   { id: '279831127',
-     username: 'mrwwwo',
-     profile_picture: 'https://scontent.cdninstagram.com/t51.2885-19/11939502_451578088380268_1972812355_a.jpg',
-     full_name: 'LW',
-     bio: '沪',
-     website: '',
-     counts: { media: 122, follows: 70, followed_by: 61 } },
-  meta: { code: 200 } }
-       { pagination: {},
-         data:
-          [array of media objects]
-            */
     });
   });
-
-  // client.get('users/self', tokenObj).then(function (user) {
-  //   console.log(user);
-  // });
 };
 
-
-function totalLikesCount(recentMedias) {
-  var totalLikes = 0;
-
+function filterByMonthYear(recentMedias) {
+  var relevantMedias = [];
   recentMedias.forEach(function (media) {
-    /**
-     { id: '658462253142614877_279831127',
-        user: [Object],
-        images: [Object],
-        created_time: '1392714839',
-        caption: null,
-        user_has_liked: false,
-        likes: [Object],
-        tags: [],
-        filter: 'Lo-fi',
-        comments: [Object],
-        type: 'image',
-        link: 'https://www.instagram.com/p/kjU-44kaNd7aaYPUz8BXN7ClYUzL_sGDvYNRU0/',
-        location: null,
-        attribution: null,
-        users_in_photo: [] },
-     **/
-
-    totalLikes += media.likes.count;
+    var mediaDate = epochToDate(media.created_time);
+    // checks the media creation date to ensure it matches the intended period.
+    if (mediaDate.month === relevantDate.month && mediaDate.year === relevantDate.year) relevantMedias.push(media);
   });
 
-  return totalLikes;
+  return countInstagramStatsFor(relevantMedias);
 
+}
+
+function countInstagramStatsFor(relevantMedias) {
+  var totalLikes = 0;
+  var totalComments = 0;
+  var totalTags = 0;
+  var totalTaggedUsers = 0;
+
+  relevantMedias.forEach(function (media) {
+    totalLikes += media.likes.count;
+    totalComments += media.comments.count;
+    totalTags += media.tags.length;
+    totalTaggedUsers += media.users_in_photo.length;
+  });
+
+  return {
+    likes: totalLikes,
+    comments: totalComments,
+    tags: totalTags,
+    taggedUsers: totalTaggedUsers,
+  }
 }
