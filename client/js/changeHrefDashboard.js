eaedbeb95c41f04e622961e8e1297e4090e843a7
Toubic <tj22fe@student.lnu.se>
2017-05-26 15:03:34 +0200

    Add more hrefs for APIs

--- client/js/changeHrefDashboard.js
+++ client/js/changeHrefDashboard.js
@@ -8,20 +8,69 @@ function changeHref() {
   var analyticsAverageTime = document.querySelector('#analytics-average-time');
   var analyticsAverageVisitedPerPages = document.querySelector('#analytics-average-visited-per-pages');
 
+  // Vilka medier och features som blivit iklickade på dashboard-sidan:
+
+  if (adwords.checked)
+      href += '&adwords=active';
+  if (adwordsClick.checked)
+      href += '&adwordsClick=active';
+  if (adwordsCpc.checked)
+      href += '&adwordsCpc=active';
+  if (adwordsViews.checked)
+      href += '&adwordsViews=active';
+
+  if (facebook.checked)
+      href += '&facebook=active';
+  if (facebookLikes.checked)
+      href += '&facebookLikes=active';
+
   if (youtube.checked)
-      href += '&youtube=active';
+    href += '&youtube=active';
   if (youtubeViews.checked)
       href += '&youtubeViews=active';
+
+  if (tynt.checked)
+      href += '&tynt=active';
+  if (tyntCopied.checked)
+      href += '&tyntCopied=active';
+
+  if (addthis.checked)
+      href += '&addthis=active';
+  if (addthisClick.checked)
+      href += '&addthisClick=active';
+
+  if (twitter.checked)
+      href += '&twitter=active';
+  if (twitterViews.checked)
+      href += '&twitterViews=active';
+
   if (analytics.checked)
       href += '&analytics=active';
   if (analyticsViews.checked)
       href += '&analyticsViews=active';
   if (analyticsUniqueViews.checked)
       href += '&analyticsUniqueViews=active';
+  if (analyticsStrongestRedirects.checked)
+      href += '&analyticsStrongestRedirects=active';
+  if (analyticsMostVisitedPages.checked)
+      href += '&analyticsMostVisitedPages=active';
   if (analyticsAverageTime.checked)
       href += '&analyticsAverageTime=active';
   if (analyticsAverageVisitedPerPages.checked)
       href += '&analyticsAverageVisitedPerPages=active';
 
+  if (linkedin.checked)
+      href += '&linkedin=active';
+  if (linkedinFollowers.checked)
+      href += '&linkedinFollowers=active';
+  if (linkedinInteractions.checked)
+      href += '&linkedinInteractions=active';
+
+  if (moz.checked)
+      href += '&moz=active';
+  if (mozKeywords.checked)
+      href += '&mozKeywords=active';
+
+  // Lägger till datan som ska skickas med på preview-knappen på dashboarden:
   previewButton.href = href;
 }
