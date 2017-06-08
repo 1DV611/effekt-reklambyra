51064ca521f8cdb9a59082dae9cfa44fc981c904
Peter Andersson <pa222ku@student.lnu.se>
2017-06-08 08:49:17 +0200

    Reject errors in getDataFor

--- server/databaseOperations/ApiData/getDataFor.js
+++ server/databaseOperations/ApiData/getDataFor.js
@@ -2,12 +2,18 @@ var mongoose = require('mongoose');
 var ApiData = require('../schemas/ApiData');
 
 function getDataFor(report) {
-  return ApiData.findOne({ report: report._id })
-    .then(function (data) {
-      return data;
-    }).catch(function (error) {
-      console.log(error);
-    });
+  return new Promise(function(resolve, reject) {
+    if (!report._id) {
+        reject("getDataFor(): report has no _id property");
+    }
+    return ApiData.findOne({ report: report._id })
+      .then(function (data) {
+        if (!data) {
+            reject('getDataFor(): No report found with id ' + report._id);
+        }
+        resolve(data);
+      }).catch(function(err) { reject(err); });
+  });
 }
 
 module.exports = getDataFor;
